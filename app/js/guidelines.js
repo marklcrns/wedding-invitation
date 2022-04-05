7ce92bfa79a7f766b7ee8a78f0d13a6f5e3d3953
Mark Lucernas <marklcrns@gmail.com>
2022-04-04 20:22:48 -0700

    fix(app): Revert lightbox js for maps

--- app/js/guidelines.js
+++ app/js/guidelines.js
@@ -71,6 +71,43 @@
         mobileMenuHide();
       }
     });
+    $('.lightbox').magnificPopup({
+      type: 'image',
+      removalDelay: 300,
+      mainClass: 'mfp-fade',
+      image: {
+        titleSrc: 'title'
+      },
+      iframe: {
+        markup: '<div class="mfp-iframe-scaler">' +
+        '<div class="mfp-close"></div>' +
+        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+        '<div class="mfp-title mfp-bottom-iframe-title"></div>' +
+        '</div>',
+        patterns: {
+          youtube: {
+            index: 'youtube.com/',
+            id: 'v=',
+            src: '//www.youtube.com/embed/%id%?autoplay=1'
+          },
+          vimeo: {
+            index: 'vimeo.com/',
+            id: '/',
+            src: '//player.vimeo.com/video/%id%?autoplay=1'
+          },
+          gmaps: {
+            index: '//maps.google.' || 'https://www.google.',
+            src: '%id%&output=embed'
+          }
+        },
+        srcAction: 'iframe_src',
+      },
+      callbacks: {
+        markupParse: function(template, values, item) {
+          values.title = item.el.attr('title');
+        }
+      },
+    });
     $('#count').countdown('2022/06/22', function(event) {
       $(this).html(event.strftime('' +
         '<div class="count-block days">%D<span class="count-label">day%!d</span></div>' +
